88c47e0ecddfd0fc3ab1e48124cc6f1ea8b128e9
Mina-W <mwaheeb@mail.lipscomb.edu>
2022-05-12 00:45:50 -0500

    added start of new water sensor implementaion

---
+++ Aquarium/floatVlaveWscreen/floatValve/README.txt
@@ -0,0 +1,4 @@
+implementation = bad
+integreated valve with lcd screen
+need to fix fliker
+need to add complixty 
--- Aquarium/floatVlaveWscreen/floatValve/floatValve.ino
+++ Aquarium/floatVlaveWscreen/floatValve/floatValve.ino
@@ -8,27 +8,30 @@ Software/Sketch: Precisely adapted – Ref: Internet
 Date: December 2015
 */
 #include "Wire.h" // For I2C
-#include "LCD.h" // For LCD
-#include "LiquidCrystal_I2C.h" // Added library*
-//Set the pins on the I2C chip used for LCD connections
-//ADDR,EN,R/W,RS,D4,D5,D6,D7
-LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7); // 0x27 is the default I2C bus address of the backpack-see article change addreess as needed, use I2C scanner
-void setup()
-{
-   // Set off LCD module
-   lcd.begin (16,2); // 16 x 2 LCD module
-   lcd.setBacklightPin(3,POSITIVE); // BL, BL_POL
-   lcd.setBacklight(HIGH);
+
+#define port              D5
+
+    /*
+  Liquid Level Float Switch
+  made on 13 oct 2020
+  by Amir Mohammad Shojaee @ Electropeak
+  Home
+*/
+void setup() {
+ pinMode(port,INPUT_PULLUP);
+ Serial.begin(9600);
 }
-void loop()
-{
-   lcd.home (); // Set cursor to 0,0
-   lcd.print("    Veronika    "); // Custom text
-   lcd.setCursor (0,1); // Go to home of 2nd line
-   lcd.print("I Love You Most");
-   //delay(1000); // Blinks of backlight
-   //lcd.setBacklight(LOW); // Backlight off
-   delay(500);
-   lcd.setBacklight(HIGH); // Backlight on
-   delay(1000);
-}
+
+void loop() {
+  
+  if(digitalRead(port)== HIGH){
+    Serial.println("On Water");
+    while(digitalRead(port)== HIGH) {}
+    delay(100);
+  }
+  else{
+    Serial.println("Outside Water");
+    while(digitalRead(port)== LOW) {}
+    delay(100); 
+  }
+  }
